e29e7c98abe650377db6ac53c7cc283b9fbf7b1b
adrian <gaticadepaz4@icloud.com>
2026-06-19 22:40:10 -0400

    Fixed index.html

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import { Analytics } from "@vercel/analytics/react"
+import { ReactNode } from "react"
 
-export default function RootLayout({ children }) {
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <body>
