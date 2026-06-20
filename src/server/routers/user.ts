262bed69cbceecdd2f74b4f98543e685aaee08ca
adrian <gaticadepaz4@icloud.com>
2026-06-20 14:45:07 -0400

    deployed

--- src/server/routers/user.ts
+++ src/server/routers/user.ts
@@ -2,8 +2,8 @@ import { createTRPCRouter, protectedProcedure } from "@/server/trpc";
 
 export const userRouter = createTRPCRouter({
   me: protectedProcedure.query(async ({ ctx }) => {
-    const user = await ctx.db.user.findUnique({
-      where: { id: ctx.session.user.id },
+    const user = await ctx.db.user.findFirst({
+      where: { accounts: { some: { providerAccountId: ctx.token.sub } } },
       select: {
         id: true,
         discordId: true,
